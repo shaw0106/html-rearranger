b2a72705338b14397daf649ba37d37c52b45a3fd
Shannon <shaw0106@algonquinlive.com>
2016-03-11 17:57:46 -0500

    Trying to get loop to work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-var $header = $('header');
+var $above = ('class')
+var $li = ('li')
+
+$('body').append('<h1>Below Ground Veggies</h1>');
+$('body').append('<h1>Above Ground Veggies');
+
+if $(li).hasClass(below) {
+
+}
+
+
+//if ($(this).hasClass('below')
+//$('ul li').each(function () {
+  //$(this).addClass('kilo');
+//});
